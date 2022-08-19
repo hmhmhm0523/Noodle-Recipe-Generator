1119a9a3e57975661a3cf0b9595cf89c18fad412
hmhmhm0523 <mhuang6@ncsu.edu>
2022-08-19 00:38:50 -0400

    Test on mobile and fix more responsive issues.

--- noodle-recipe-generator/src/pages/Home.js
+++ noodle-recipe-generator/src/pages/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
             </h1>
             <h4>PRESENTED BY MENG HUANG</h4>
             <p>At my home in Hunan China, you can eat noodles for breakfast, dinner, and supper. Almost everyone from Hunan has a story about the noodle restaurant downstairs. This recipe generator is created to help make a bowl of rice noodles in my hometown style.</p>
-            <div>Demo Video</div>
+            <div><a href="www.Youtube.com">Demo Video</a></div>
             <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
             <Link to="buildRecipe" className="ButtonPanel"><Button >GET STARTED</Button> </Link>
         </Card>
